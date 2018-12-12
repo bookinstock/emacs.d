f46fbfa4f0d657606d0227157e95796fa9c4dc98
Wende Lu <wendelu1991@gmail.com>
2018-12-12 14:47:41 +0800

    add more config

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -9,15 +9,15 @@
 (global-set-key (kbd "C-0") 'next-buffer)
 
 ;;; disable auto save file
-;(setq auto-save-default nil)
+(setq auto-save-default nil)
 
 ;;; fast open init config
-;(defun open-init-file()
-;  (interactive)
-;  (find-file "~/.emacs.d/init.el"))
-;(global-set-key (kbd "<f1>") 'open-init-file)
+(defun open-init-file()
+  (interactive)
+  (find-file "~/.emacs.d/init.el"))
+(global-set-key (kbd "<f1>") 'open-init-file)
 
 ;;; set default font
-;;;(set-face-attribute 'default nil :family "Menlo" :height 140 :weight 'normal)
+(set-face-attribute 'default nil :family "Menlo" :height 200 :weight 'normal)
 
 (provide 'init-local)
