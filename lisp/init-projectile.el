29ff80473518b987e80ac2ba58d10f84a55cb818
Wende Lu <wendelu1991@gmail.com>
2018-12-12 16:03:03 +0800

    customize init projectile

--- lisp/init-projectile.el
+++ lisp/init-projectile.el
@@ -5,9 +5,10 @@
   (setq-default projectile-mode-line-prefix " Proj")
 
   (after-load 'projectile
-    (define-key projectile-mode-map (kbd "C-c C-p") 'projectile-command-map))
+    (setq projectile-globally-ignored-directories (append '("log") projectile-globally-unignored-directories))
+    (setq projectile-globally-ignored-files (append '("*.log" "*.json") projectile-globally-ignored-files))
+    (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map))
 
   (maybe-require-package 'ibuffer-projectile))
-
 
 (provide 'init-projectile)
